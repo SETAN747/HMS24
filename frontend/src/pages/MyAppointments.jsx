ae0e09a771b81831d6588b98433c9f0e7f1c7abb
Utkarsh Mehta <utkarshmehta777@gmail.com>
2025-09-05 17:23:30 +0530

    Payment status bug fix

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -154,7 +154,7 @@ const MyAppointments = () => {
                   Paid
                 </button>
               )}
-              {!item.cancelled && !item.isCompleted &&( <button  onClick={() => appointmentRazorpay(item?._id)} className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-customPrimary hover:text-white transition-all duration-300 ">Pay Online</button>)}
+              {!item.cancelled && !item.payment && !item.isCompleted &&  ( <button  onClick={() => appointmentRazorpay(item?._id)} className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-customPrimary hover:text-white transition-all duration-300 ">Pay Online</button>)}
               {!item.cancelled && !item.isCompleted && ( <button  onClick={() => cancelAppointment(item._id)} className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300" >Cancel Appointment </button>)}
             {item.cancelled && !item.isCompleted && (
                 <button className="sm:min-w-48 py-2 border border-red-500 rounded text-red-500">
